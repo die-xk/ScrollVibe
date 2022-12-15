1101ed17366b336aa08ead25b8eda6116511b500
Diellon Bytyqi <diellonbytyqi14@gmail.com>
2022-12-15 19:24:31 +0100

    new

--- routes/connect.js
+++ routes/connect.js
@@ -1,19 +1,3 @@
-function checkAdminRights(){
-  var adminEmail = "diellonbytyqi14@gmail.com"
-  var adminPassword = "h72hn4dnqx"
-  let a = document.forms["myForm"]["email"].value;
-  let b = document.forms["myForm"]["password"].value;
-  if(a = adminEmail){
-    if(b = adminPassword){
-    // Set the new URL to redirect the user to
-    var newUrl = 'http://127.0.0.1:3000/index.html';
-    // Redirect the user to the new URL
-    window.location.replace(newUrl);
-    }else{
-      return validateForm();
-    }
-  }
-}
 function validateForm() {
   let x = document.forms["myForm"]["email"].value;
   let y = document.forms["myForm"]["password"].value;
